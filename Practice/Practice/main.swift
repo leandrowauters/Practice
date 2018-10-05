73a5e9dda1450a5548fced30edbc2fb72de7dbbb
John Doe <leandro.wauters@gmail.com>
2018-10-05 15:34:15 -0400

    initial comit

---
+++ Practice/Practice/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Practice
+//
+//  Created by Leandro Wauters on 10/5/18.
+//  Copyright © 2018 Leandro Wauters. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let userName = (firstname: "leandro", age: 30, city: "NYC")
+print(userName.firstname, userName.age, userName.city)
